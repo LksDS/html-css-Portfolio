d287716d33df0d6c96d29251da6175948828c854
Lucas Diniz <lucasds.032@gmail.com>
2024-11-02 23:45:38 -0300

    Fixed some bugs and added some missing features

--- script.js
+++ script.js
@@ -34,4 +34,35 @@ const observer = new IntersectionObserver((entries) => {
     togglesidebar()
   });
 
+  document.addEventListener('DOMContentLoaded',function(event){
+    var dataText = [ "Lucas Diniz",""];
+    
+    function typeWriter(text, i, fnCallback) {
+      if (i < (text.length)) {
+       document.querySelector("#efeito-escrita").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
   
+        setTimeout(function() {
+          typeWriter(text, i + 1, fnCallback)
+        }, 150);
+      }
+      else if (typeof fnCallback == 'function') {
+        setTimeout(fnCallback, 700);
+      }
+    }
+     function StartTextAnimation(i) {
+      if (i < dataText[i].length) {
+       typeWriter(dataText[i], 0, function(){
+         StartTextAnimation(i + 1);
+       });
+      }else{
+        typeWriter(dataText[0], 0, function(){
+          setTimeout(function() {
+            StartTextAnimation(0);
+          }, 1000);
+
+        });
+      }
+    }
+    StartTextAnimation(0);
+    
+  });
